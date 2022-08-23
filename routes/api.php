1b23f19c589804ec4a6cbec6b094572a1a9080ea
Herry Prasetyo <herry081288@gmail.com>
2022-08-23 13:17:25 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//rute
+Route::get('hellojson', function () {
+    return response()->json([
+        'msg' => 'success',
+        'data' => [
+            'nama' => 'isi nama kalian',
+            'kelas' => 'isi kelas kalian',
+            'absen' => '2022-08-23',
+            'ortu' => [
+                'ayah' => 'ayah kalian',
+                'ibu' => 'ibu kalian'
+            ]
+        ]
+    ]);
+});
